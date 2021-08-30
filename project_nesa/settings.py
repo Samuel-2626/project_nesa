c39b0e31c3f42e83e84e84cd6c03f03e79a5f4fd
Samuel Torimiro <efosonaname@gmail.com>
2021-08-30 16:36:39 +0100

    updated settings.py

--- project_nesa/settings.py
+++ project_nesa/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = ['www.nesaacademy.com', '127.0.0.1']
+ALLOWED_HOSTS = ['www.nesaacademy.com', 'nesaacademy.com', '127.0.0.1']
 
 
 # Application definition
