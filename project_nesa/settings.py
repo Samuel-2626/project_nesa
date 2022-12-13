77989a7815e29ddaac9e51ca69bc08ced668cf5b
Samuel Torimiro <efosonaname@gmail.com>
2022-12-13 09:51:20 +0100

    changed domain name

--- project_nesa/settings.py
+++ project_nesa/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = ['nesaacademy.herokuapp.com',
+ALLOWED_HOSTS = ['projectnesa-production.up.railway.app',
                  'www.nesaacademy.com', 'nesaacademy.com', '127.0.0.1']
 
 
--- questions/models.py
+++ questions/models.py
@@ -11,69 +11,65 @@ from taggit.managers import TaggableManager
 from django.shortcuts import reverse
 
 
-
 class PublishedManager(models.Manager):
-  def get_queryset(self):
-    return super(PublishedManager, self).get_queryset()\
-      .filter(status='published')
-
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
 
 
 class Question(models.Model):
 
-  STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-  )
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
 
-  """ A Model To enable Authenticated Users to Post Ask Question """
-  
-  title = models.CharField(max_length=100, help_text='Maximum of 100 characters')
-  slug = AutoSlugField(populate_from='title', unique=True)
-  author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='my_questions', null=True)
-  body = RichTextUploadingField(help_text="Explain your question")
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  counter = models.BigIntegerField(default=0)
-  views = models.PositiveIntegerField(default=0)
+    """ A Model To enable Authenticated Users to Post Ask Question """
 
+    title = models.CharField(max_length=100, help_text="Maximum of 100 characters")
+    slug = AutoSlugField(populate_from="title", unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="my_questions", null=True
+    )
+    body = RichTextUploadingField(help_text="Explain your question")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    counter = models.BigIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
 
-  status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
-  tags = TaggableManager(help_text='A list of comma separated values. e.g healtheconomics, macroeconomics, etc.')
+    tags = TaggableManager(
+        help_text="A list of comma separated values. e.g healtheconomics, macroeconomics, etc."
+    )
 
-  class Meta:
-    ordering = ('-created',)
+    class Meta:
+        ordering = ("-created",)
 
-  def __str__(self):
-    return f'Qusetion by {self.author}'
+    def __str__(self):
+        return f"Qusetion by {self.author}"
 
-  objects = models.Manager()
-  published = PublishedManager()
+    objects = models.Manager()
+    published = PublishedManager()
 
-  def get_absolute_url(self):
-      return reverse("questions:question-detail", args=[self.slug])
-
+    def get_absolute_url(self):
+        return reverse("questions:question-detail", args=[self.slug])
 
 
 class QuestionViews(models.Model):
 
-  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_views')
-  host = models.CharField(max_length=500)
-  ip = models.GenericIPAddressField()
-  created = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="question_views"
+    )
+    host = models.CharField(max_length=500)
+    ip = models.GenericIPAddressField()
+    created = models.DateTimeField(auto_now_add=True)
 
-  class Meta:
-    ordering = ('-created',)
-    unique_together = ['question', 'ip']
+    class Meta:
+        ordering = ("-created",)
+        unique_together = ["question", "ip"]
 
-  def __str__(self):
-      return self.ip
-  
-
-
-
-
+    def __str__(self):
+        return self.ip
 
 
 # class CommentQuestion(models.Model):
@@ -92,77 +88,84 @@ class QuestionViews(models.Model):
 
 
 class Answer(models.Model):
-  
-  STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-  )
 
-  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-  author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='my_answers', null=True)
-  answer = RichTextUploadingField()
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  accepted = models.BooleanField(default=False)
-  counter = models.BigIntegerField(default=0)
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
 
-  status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="my_answers", null=True
+    )
+    answer = RichTextUploadingField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    accepted = models.BooleanField(default=False)
+    counter = models.BigIntegerField(default=0)
 
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
-  class Meta:
-    ordering = ('-counter',)
-    unique_together = ['question', 'author']
+    class Meta:
+        ordering = ("-counter",)
+        unique_together = ["question", "author"]
 
-  def __str__(self):
-    return f'Answer by {self.author} on {self.question}'
+    def __str__(self):
+        return f"Answer by {self.author} on {self.question}"
 
-  objects = models.Manager()
-  published = PublishedManager()
+    objects = models.Manager()
+    published = PublishedManager()
 
 
 class QuestionVotes(models.Model):
 
-  STATUS_CHOICES = (
-    ('upvote', 'Upvote'),
-    ('downvote', 'Downvote'),
-  )
+    STATUS_CHOICES = (
+        ("upvote", "Upvote"),
+        ("downvote", "Downvote"),
+    )
 
-  author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='my_vote_questions', null=True)
-  question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='questions_votes')
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="my_vote_questions", null=True
+    )
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="questions_votes"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
-  class Meta:
-    ordering = ('-created',)
- 
+    class Meta:
+        ordering = ("-created",)
 
-  def __str__(self):
-    return f'Vote by {self.author} on {self.question}'
-    
+    def __str__(self):
+        return f"Vote by {self.author} on {self.question}"
 
 
 class VoteAnswer(models.Model):
 
-  STATUS_CHOICES = (
-    ('upvote', 'Upvote'),
-    ('downvote', 'Downvote'),
-  )
+    STATUS_CHOICES = (
+        ("upvote", "Upvote"),
+        ("downvote", "Downvote"),
+    )
 
-  answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answers_votes')
-  author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='my_vote_answers', null=True)
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    answer = models.ForeignKey(
+        Answer, on_delete=models.CASCADE, related_name="answers_votes"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="my_vote_answers", null=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
+    class Meta:
+        ordering = ("-created",)
+        unique_together = ["answer", "author"]
 
-  class Meta:
-    ordering = ('-created',)
-    unique_together = ['answer', 'author']
-
-  def __str__(self):
-    return f'Vote by {self.author} on {self.answer}'
-
+    def __str__(self):
+        return f"Vote by {self.author} on {self.answer}"
 
 
 # class CommentAnswer(models.Model):
@@ -178,4 +181,3 @@ class VoteAnswer(models.Model):
 
 #   def __str__(self):
 #     return f'Comment by {self.author} on {self.answer}'
-
