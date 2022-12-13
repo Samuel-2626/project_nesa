2ccdff1a3ac80e9e5008b4e69ed78e483e1c5bbb
Samuel Torimiro <efosonaname@gmail.com>
2022-12-13 11:52:05 +0100

    changed domain name

--- project_nesa/settings.py
+++ project_nesa/settings.py
@@ -18,8 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = ['projectnesa-production.up.railway.app',
-                 'www.nesaacademy.com', 'nesaacademy.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
